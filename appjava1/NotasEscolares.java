74ae7dd0be494032208c55a2d1c28d2153f74182
Josmar Aron Falcade <182310075@senacinformatica.br>
2024-09-02 10:26:26 -0300

    teste

--- appjava1/NotasEscolares.java
+++ appjava1/NotasEscolares.java
@@ -33,5 +33,6 @@ public class NotasEscolares {
     }
     System.out.println(".:Sistema de notas Encerrado:.")
     System.out.println("Alterado no VS online")
+    System.out.println("Teste de push")
 
 }
